800dd5d92f3cdd1af1ad83e39ba046d3e8d9bb60
trup922 <trup922@gmail.com>
2022-06-24 13:24:08 +0530

    settting brower thr maven

--- src/test/java/factory/DriverManager.java
+++ src/test/java/factory/DriverManager.java
@@ -6,23 +6,25 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverManager {
 	WebDriver driver;
-	
+
 	public WebDriver initDriver(String browser) {
-		//String browser=System.getProperty("browser","chrome");// it will take property through maven
-		switch(browser.toUpperCase()) {
+		if (browser == null) {
+			browser = System.getProperty("browser", "chrome");// it will take property through maven
+		}
+		switch (browser.toUpperCase()) {
 		case "CHROME":
-			driver=WebDriverManager.chromedriver().create();
+			driver = WebDriverManager.chromedriver().create();
 			break;
 		case "FIREFOX":
-			driver=WebDriverManager.firefoxdriver().create();
+			driver = WebDriverManager.firefoxdriver().create();
 			break;
 		case "EDGE":
-			driver=WebDriverManager.edgedriver().create();
+			driver = WebDriverManager.edgedriver().create();
 			break;
 		default:
-			throw new IllegalStateException("Invalid browser name - "+browser);
+			throw new IllegalStateException("Invalid browser name - " + browser);
 		}
-		
+
 		return driver;
 	}
 
