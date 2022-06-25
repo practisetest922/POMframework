1309df25e69dccc553ea97f0cbf8cd8804e954f8
trup922 <trup922@gmail.com>
2022-06-25 14:24:28 +0530

    Update Logintest.java

--- src/test/java/tests/Logintest.java
+++ src/test/java/tests/Logintest.java
@@ -33,5 +33,17 @@ public class Logintest extends BaseTest {
 		
 	}
 	
+	@Test(description = "Login and verify yellow ")
+	@Description("Searches for red ")
+	public void thirdTest() {
+		HomePage hm=new HomePage(getDriver());
+//		hm.openUrl();
+		hm.clickbuttonShopnow();
+		StorePage sp=new StorePage(getDriver());
+		sp.search("red");
+		assertEquals("Yellow",sp.verifyRed());
+		
+	}
+	
 	
 }
