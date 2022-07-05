04b6dc2775c6c8f7184bf5c4cc7b4ff9dc25a5ee
trup922 <practisetest922@gmail.com>
2022-07-05 10:36:21 +0530

    added new testcase to check failure

--- src/test/java/tests/Logintest.java
+++ src/test/java/tests/Logintest.java
@@ -2,12 +2,15 @@ package tests;
 
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
 import io.qameta.allure.Description;
 import page.HomePage;
 import page.StorePage;
+
+
 
 public class Logintest extends BaseTest {
 	
@@ -30,6 +33,18 @@ public class Logintest extends BaseTest {
 		StorePage sp=new StorePage(getDriver());
 		sp.search("red");
 		assertEquals("Red Shoes",sp.verifyRed());
+		
+	}
+	
+	@Test(description = "Login and verify yellow ")
+	@Description("Searches for yellow ")
+	public void ThirdTest() {
+		HomePage hm=new HomePage(getDriver());
+//		hm.openUrl();
+		hm.clickbuttonShopnow();
+		StorePage sp=new StorePage(getDriver());
+		sp.search("yellow");
+		assertEquals("yellow Shoes",sp.verifyRed());
 		
 	}
 	
